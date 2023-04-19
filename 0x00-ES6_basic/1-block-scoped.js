0a9875470ed7ffd441acdba00d10b59faef4837c
Ian Edwin Kitembe <ianedwin@outlook.com>
2023-04-19 15:56:13 +0300

    eslint --fix

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,9 +1,9 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
+  const task = false;
   const task2 = true;
 
   if (trueOrFalse) {
-    task = true;
+    // no overwriting
   }
 
   return [task, task2];
