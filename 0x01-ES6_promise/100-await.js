17430265005ceecd1f3f718304df61f4201784a4
Ian Edwin Kitembe <ianedwin@outlook.com>
2023-04-19 21:23:15 +0300

    async -> await

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,16 @@
+import { uploadPhoto, createUser } from "./utils.js";
+
+async function asyncUploadUser() {
+  try {
+    const [photoResponse, userResponse] = await Promise.all([
+      uploadPhoto(),
+      createUser(),
+    ]);
+    return { photo: photoResponse, user: userResponse };
+  } catch (error) {
+    console.error(error);
+    return { photo: null, user: null };
+  }
+}
+
+export default asyncUploadUser;
