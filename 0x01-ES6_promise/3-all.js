ed0f7530dfeee2057b2ef842bdffa6492c8c0d45
Ian Edwin Kitembe <ianedwin@outlook.com>
2023-04-19 17:06:04 +0300

    from utils handle profile signup

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,13 +1,9 @@
 import { uploadPhoto, createUser } from './utils';
 
-function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
-    .then(([photoResponse, userResponse]) => {
-      console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
-    })
-    .catch(() => {
-      console.log('Signup system offline');
-    });
+export default function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()]).then(([photoResponse, userResponse]) => {
+    console.log(`${photoResponse.body}, ${userResponse.firstName}, ${userResponse.lastName}`);
+  }).catch(() => {
+    console.log("Signup system offline");
+  });
 }
-
-export default handleProfileSignup;
