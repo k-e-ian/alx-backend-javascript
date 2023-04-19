6247e9942d2de468a32959342fbe534f007e0e63
Ian Edwin Kitembe <ianedwin@outlook.com>
2023-04-19 16:59:18 +0300

    then catch all errors

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,9 +1,13 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => {
-    console.log('Got a response from the API');
-    return { status: 200, body: 'success' };
-  }).catch(() => {
-    console.log('Got an error response from the API');
-    return new Error();
+  return new Promise((resolve, reject) => {
+    promise.then(() => {
+      console.log('Got a response from the API');
+      resolve({ status: 200, body: 'success' });
+    }).catch(() => {
+      console.log('Got an error response from the API');
+      reject(new Error());
+    }).finally(() => {
+      console.log('Got a response from the API');
+    });
   });
 }
