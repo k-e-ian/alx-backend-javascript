fb95a43e1470d631ccd107a0d366019735e9175e
Ian Edwin Kitembe <ianedwin@outlook.com>
2023-04-19 15:28:46 +0300

    let and const usage

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
+  let task = false;
+  let task2 = true;
 
   if (trueOrFalse) {
-    const newTask = true;
-    const newTask2 = false;
-    return [newTask, newTask2];
+    task = true;
+    task2 = false;
   }
 
   return [task, task2];
