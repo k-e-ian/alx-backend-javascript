e75f3aab8bc04cc3fc44170c365d33ca2de37b68
Ian Edwin Kitembe <ianedwin@outlook.com>
2023-04-20 21:36:14 +0300

    Inheritance of classes

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -13,7 +13,7 @@ export default class Building {
   // Abstract method
 
   evacuationWarningMessage() {
-    if (this.constructor === Building) {
+    if (this.constructor === Building && typeof this.evacuationWarningMessage !== 'function') {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
   }
---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,19 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    if (typeof floors !== 'number' || floors <= 0) {
+      throw new TypeError('Floors must be a positive number');
+    }
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors.`;
+  }
+}
