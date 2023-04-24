43bc04fa297a8c37d07152c7205c408ae6ffd708
Ian Edwin Kitembe <ianedwin@outlook.com>
2023-04-24 15:29:07 +0300

    cleanset returned

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,15 @@
+export default function cleanSet(set, startString) {
+  let result = '';
+
+  for (const value of set) {
+    if (value.startsWith(startString)) {
+      result += `${value.substring(startString.length)}-`;
+    }
+  }
+
+  if (result.length > 0) {
+    result = result.substring(0, result.lenght - 1);
+  }
+
+  return result;
+}
