c9233b1e7b49e0cd5874094e69aceb025357acb5
Ian Kitembe <ianedwin@outlook.com>
2023-06-13 23:22:45 +0000

    1stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,12 +3,13 @@
  */
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', (input) => {
-  const name = input.toString().trim();	
-  if (name !== '') {
-    process.stdout.write(`Your name is: ${name}\n`);
-  }
+process.stdin.once('data', (data) => {
+  const name = data.toString().trim();
+  process.stdout.write(`Your name is: ${name}\n`);
+  process.exit();
 });
 
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
+  process.exit();
+});
