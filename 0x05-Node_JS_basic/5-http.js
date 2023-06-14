49b8667197409d7073be578509dc86a11d88bd04
Ian Kitembe <ianedwin@outlook.com>
2023-06-14 11:57:39 +0000

    more complex

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,32 +1,32 @@
 const http = require('http');
 const countStudents = require('./3-read_file_async');
 
-const databasePath = process.argv[2];
+const PORT = 1245;
 
 const app = http.createServer((req, res) => {
-  const { method, url } = req;
-
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-
-  if (method === 'GET' && url === '/') {
-    res.end('Hello Holberton School!\n');
-  } else if (method === 'GET' && url === '/students') {
-    res.write('This is the list of our students\n');
-
-    countStudents(databasePath)
-      .then(() => {
-        res.end();
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    countStudents('database.csv')
+      .then((data) => {
+        const response = `This is the list of our students\n${data}`;
+        res.writeHead(200, { 'Content-Type': 'text/plain', 'Content-Length': response.length });
+        res.end(response);
       })
       .catch((error) => {
-        res.end(error.message);
+        console.error(error); // Log the error to the console
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Cannot load the database');
       });
   } else {
-    res.statusCode = 404;
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
     res.end('Not found');
   }
 });
 
-app.listen(1245);
+app.listen(PORT, () => {
+  console.log(`Server is running and listening on port ${PORT}`);
+});
 
 module.exports = app;
