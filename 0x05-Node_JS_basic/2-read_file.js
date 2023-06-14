8874dd968819944f8f0cc045fed62ec69c76d1c0
Ian Kitembe <ianedwin@outlook.com>
2023-06-14 00:04:22 +0000

    synchronous file reading

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -3,14 +3,17 @@ const fs = require('fs');
 function countStudents(path) {
   try {
     const data = fs.readFileSync(path, 'utf8');
-    const lines = data.split('\n').filter((line) => line !== '');
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
 
     const students = lines.map((line) => line.split(','));
 
     const fields = {};
     students.forEach((student) => {
-      const field = student[3];
-      if (field in fields) {
+      const field = student[3].trim();
+      if (field === '') {
+        return; // Skip empty field
+      }
+      if (Object.prototype.hasOwnProperty.call(fields, field)) {
         fields[field].count += 1;
         fields[field].students.push(student[0]);
       } else {
@@ -21,15 +24,18 @@ function countStudents(path) {
       }
     });
 
-    const totalStudents = students.length;
+    const totalStudents = Object.values(fields)
+      .map((field) => field.count)
+      .reduce((sum, count) => sum + count, 0);
 
     console.log(`Number of students: ${totalStudents}`);
+    delete fields.field; // Remove the unwanted "field" entry
     for (const field in fields) {
-      console.log(
-        `Number of students in ${field}: ${fields[field].count}. List: ${fields[field].students.join(
-          ', '
-        )}`
-      );
+      if (Object.prototype.hasOwnProperty.call(fields, field)) {
+        console.log(
+          `Number of students in ${field}: ${fields[field].count}. List: ${fields[field].students.join(', ')}`,
+        );
+      }
     }
   } catch (error) {
     throw new Error('Cannot load the database');
